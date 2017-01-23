7518a1a5f64e18b24b8e209165d643e5d447f686
Suchita Mohanty <mohanty.suchita@gmail.com>
2017-01-23 08:50:56 +0530

    Creation of an employee object with a nested object in Java Script

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Assignment – Create an object Employee. Store name, age and
+salary and address. Address property should again be a nested
+object which stores city, state and pincode. Display the value of
+all properties of Employee object in console.
---
+++ js/app.js
@@ -0,0 +1,19 @@
+var employee = {
+  name: "Suchita",
+  age: 30,
+  salary: 50000,
+  address: {
+    city : "Banglore",
+    state : "Karnatak",
+    pincode : 560076
+  }
+};
+
+console.log ('Employee Details');
+console.log ('Name: ' + employee.name);
+console.log ('Aame: ' + employee.age);
+console.log ('Salary: ' + employee.salary);
+console.log ('Address:');
+console.log ('City: ' + employee.address.city);
+console.log ('State: ' + employee.address.state);
+console.log ('Pincode: ' + employee.address.pincode);
